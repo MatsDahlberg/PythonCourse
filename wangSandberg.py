b320d21c87c6850888541b807529abd4b7c47dc6
Mats Dahlberg <mats.dahlberg@scilifelab.se>
2013-01-21 16:08:05 +0100

    Typos

--- wangSandberg.py
+++ wangSandberg.py
@@ -2,6 +2,7 @@ import sqlite3 as lite
 import sys
 import csv
 import numpy as np
+import scipy
 
 
 # Create a connection to the database
@@ -21,11 +22,12 @@ data = cur.fetchone()
 print "SQLite version: %s" % data 
 
 
-# This is what the 2 first lines of the "raw data" looks like:
-#Gene   UHRLowcov	brainLowcov	adipose	brain	breast	colon	heart	liver	lymphNode	skelMuscle	testes	cerebellum1	cerebellum2	cerebellum3	cerebellum4	cerebellum5	cerebellum6	MCF7	BT474	HME	MB435	T47D	Symbol	Description
-#ENSG00000000003	18.51	1.33	5.91	2.41	4.25	3.90	2.51	17.43	1.40	0.51	8.95	0.58	0.33	0.35	0.36	0.33	0.44	7.25	0.81	3.09	5.02	3.00	TSPAN6	transmembrane 4 superfamily member 6
-# ... 23115 times
-
+"""This is what the 2 first lines of the "raw data"  (the file WangSandberg.rpkm.txt) looks like:
+Gene            UHRLowcov	brainLowcov	adipose	brain	breast	colon	heart	liver	lymphNode	skelMuscle	testes	cerebellum1	cerebellum2	cerebellum3	cerebellum4	cerebellum5	cerebellum6	MCF7	BT474	HME	MB435	T47D	Symbol	Description
+ENSG00000000003	18.51	        1.33	        5.91	2.41	4.25	3.90	2.51	17.43	1.40	        0.51	        8.95	0.58	        0.33	        0.35	        0.36	        0.33	        0.44	        7.25	0.81	3.09	5.02	3.00	TSPAN6	transmembrane 4 superfamily member 6
+ ... 23115 times
+0               1               2               3       4       5       6       7       8       9               10              11      12              13              14              15              16              17              18      19      20      21      22      23      24
+"""
 # Creat a table that looks as your data ( I am skipping the description column)
 cur.execute("""CREATE TABLE wangsandberg (
   ensg TEXT,
@@ -95,8 +97,8 @@ print len(rows)
 print rows[0][0]
 
 cur.execute('select * from wangsandberg where brain > 10 order by brain desc' )
-# Sort the table on the highest rpkm value for the brain tissue, require that the
-# rpkm in brain should be more than 10
+# Select the rows with rpkm > 10 for brain and sort the result with higest brain rpkm at the top
+
 rows = cur.fetchall()
 print len(rows)
 print rows[0]
@@ -142,25 +144,28 @@ rows = cur.fetchall()
 
 iCount = 0
 lenOfRow = len(rows[0])
-for row in rows:         # This loop will calculate normalized values and insert the new data into the new columns we created
+for row in rows:
+    # This loop will calculate normalized rpkm values for each gene
+    # and insert the result into the new columns we just created
     iCount += 1
-    if (iCount % 100) == 0:
-        # Commit everey 100 insert to spare the redo log
+    if (iCount % 500) == 0:
+        # Commit everey 500 insert to spare the redo log
         con.commit()
         print iCount
-    fMax = np.asarray(row[1:lenOfRow]).max()      # higest rpkm value for this gene
+    fMax = np.asarray(row[1:lenOfRow]).max()      # highest rpkm value for this gene
     if fMax == 0:
         # If there are no values for the gene, then we skip it and take the next one
         continue
     row2 = [float(x) for x in row[1:lenOfRow]]  # copy the values for this gene
     row2 /= fMax     # Divide all values with highest
     # Update the database with the normalized values
-    sSql = """update wangsandberg set uhrlowcov_rel='%s', brainlowcov_rel='%s', adipose_rel='%s', brain_rel='%s', breast_rel='%s',
-    colon_rel='%s', heart_rel='%s', liver_rel='%s', lymphnode_rel='%s', skelmuscle_rel='%s', testes_rel='%s', cerebellum1_rel='%s',
-    cerebellum2_rel='%s', cerebellum3_rel='%s', cerebellum4_rel='%s', cerebellum5_rel='%s', cerebellum6_rel='%s', mcf7_rel='%s',
-    bt474_rel='%s', hme_rel='%s', mb435_rel='%s', t47d_rel='%s' where ensg = '%s'
-    """ % (row2[0], row2[1], row2[2], row2[3], row2[4], row2[5], row2[6], row2[7], row2[8], row2[9], row2[10], row2[11], row2[12],
-    row2[13], row2[14], row2[15], row2[16], row2[17], row2[18], row2[19], row2[20], row2[21], row[0])
+    sSql = """update wangsandberg set uhrlowcov_rel='%s', brainlowcov_rel='%s', adipose_rel='%s', brain_rel='%s',
+    breast_rel='%s', colon_rel='%s', heart_rel='%s', liver_rel='%s', lymphnode_rel='%s', skelmuscle_rel='%s',
+    testes_rel='%s', cerebellum1_rel='%s', cerebellum2_rel='%s', cerebellum3_rel='%s', cerebellum4_rel='%s',
+    cerebellum5_rel='%s', cerebellum6_rel='%s', mcf7_rel='%s', bt474_rel='%s', hme_rel='%s', mb435_rel='%s',
+    t47d_rel='%s' where ensg = '%s' """ % (row2[0], row2[1], row2[2], row2[3], row2[4], row2[5], row2[6],
+    row2[7], row2[8], row2[9], row2[10], row2[11], row2[12], row2[13], row2[14], row2[15], row2[16], row2[17],
+    row2[18], row2[19], row2[20], row2[21], row[0])
     slask = cur.execute(sSql)
 # Commit the last bunch of inserts
 con.commit()
@@ -171,3 +176,34 @@ cur.execute('select gene_name, ensg, liver, liver_rel from wangsandberg where li
 rows = cur.fetchall()
 print len(rows)
 print rows[0]
+
+
+# Export the normalized values to a text file
+
+sSql = """select uhrlowcov_rel, brainlowcov_rel, adipose_rel, brain_rel, breast_rel,
+    colon_rel, heart_rel, liver_rel, lymphnode_rel, skelmuscle_rel, testes_rel, cerebellum1_rel,
+    cerebellum2_rel, cerebellum3_rel, cerebellum4_rel, cerebellum5_rel, cerebellum6_rel,
+    mcf7_rel, bt474_rel, hme_rel, mb435_rel, t47d_rel from wangsandberg order by ensg""" 
+
+# Fetch all the data in the table
+cur.execute(sSql)
+rows = cur.fetchall()
+
+iNrOfGenes = len(rows)
+sSamples = "uhrlowcov_rel,brainlowcov_rel,adipose_rel,brain_rel,breast_rel,colon_rel,heart_rel,liver_rel,lymphnode_rel,skelmuscle_rel,testes_rel,cerebellum1_rel,cerebellum2_rel,cerebellum3_rel,cerebellum4_rel,cerebellum5_rel,cerebellum6_rel,mcf7_rel,bt474_rel,hme_rel,mb435_rel,t47d_rel"
+arraySamples = sSamples.split(',')
+iNrOfSamples = len(arraySamples)
+D = scipy.zeros([iNrOfSamples, iNrOfGenes])
+
+for i in range(iNrOfSamples):
+    for j in range(iNrOfGenes):
+        D[i, j] = rows[j][i]
+
+
+with open('normalizedRPKM.csv', 'wb') as outputFile:
+    for i in range(iNrOfSamples):
+        sTmpStr = []
+        sTmpStr.append([str(x) for x in D[i]])
+        sStr = arraySamples[i] + ';' + ';'.join(sTmpStr[0])
+        outputFile.write(sStr + '\n')
+outputFile.close()
